a683ca55ecd0b4489618efcbd1f7dd9e79117722
mg343 <milu.garimella@gmail.com>
2022-09-25 19:58:51 -0400

    bacteria with most samples found

--- eda.py
+++ eda.py
@@ -1,32 +1,28 @@
 import os
-
-path = "C:\\Users\milug\\Mihir's Important Stuff\\Mihir's Visual Studio\\NHSEE"
- 
-# to store files in a list
-list = []
- 
-# dirs=directories
 import os.path
-dirs = [d for d in os.listdir("C:\\Users\milug\\Mihir's Important Stuff\\Mihir's Visual Studio\\NHSEE\\2006\\2006") if os.path.isdir(os.path.join("C:\\Users\milug\\Mihir's Important Stuff\\Mihir's Visual Studio\\NHSEE\\2006\\2006", d))]
+dirs = [d for d in os.listdir("C:\\Users\\milug\\Mihir's Important Stuff\\Mihir's Visual Studio\\NHSEE\\Phytoplankton Detection (10)\\2006\\2006") if os.path.isdir(os.path.join("C:\\Users\\milug\\Mihir's Important Stuff\\Mihir's Visual Studio\\NHSEE\\Phytoplankton Detection (10)\\2006\\2006", d))]
 
-readable_sizes = {'B': 1,
-                'KB': float(1) / 1024,
-                'MB': float(1) / (1024 * 1024),
-                'GB': float(1) / (1024 * 1024 * 1024)
-                    }
-
+numfiles = 0
 dir_size = 0
 for folder in dirs:
-    direct = "C:\\Users\milug\\Mihir's Important Stuff\\Mihir's Visual Studio\\NHSEE\\2006\\2006\\" + folder
+    direct = "C:\\Users\\milug\\Mihir's Important Stuff\\Mihir's Visual Studio\\NHSEE\\Phytoplankton Detection (10)\\2006\\2006\\" + folder
     direct = str(direct)
     for (path, dirs, file1) in os.walk(direct):
         for file in file1:
             filename = os.path.join(path, file)
             dir_size += os.path.getsize(filename)
+            numfiles += 1
 
-    if dir_size == 0:
-        print("Folder ", folder, "Empty")
-    else:
-        print("Folder ", folder, "Size: ", dir_size)
+    if numfiles > 500:
+        print("Folder", folder, "Size:", numfiles)
+    # if dir_size == 0:
+    #     print(folder + " -----------------------------------------Empty")
+    # elif numfiles > 2000:
+    #     print("\n")
+    #     print("Folder ", folder, "Size: ", numfiles)
+    #     print("\n")
+    # else:
+    #     print("Folder ", folder, "Size: ", numfiles)
 
     dir_size = 0
+    numfiles = 0
