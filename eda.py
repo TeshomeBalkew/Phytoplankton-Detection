4f3239743c5ca57a2f9e2c65f3793f884190f51b
mg343 <milu.garimella@gmail.com>
2022-10-08 11:17:20 -0400

    moved all eda code into eda folder

--- eda.py
+++ eda.py
@@ -1,3 +1,65 @@
+import pandas as pd
+import cv2
+import os
+import os.path
+import time
+import numpy as np
+
+pixeldatalist = []
+actvalues = []
+dirs = ['Chaetoceros', 'Skeletonema', 'Thalassionema', 'Guinardia_delicatula', 'Leptocylindrus', 'Mesodinium_sp', 'mix']
+
+numfiles = 0
+dir_size = 0
+
+colname = []
+for val in range(784):
+        colname.append(val)
+colname.append('label')
+datan = pd.DataFrame(columns = colname)
+
+
+for folder in dirs:
+    direct = "C:\\Users\\milug\\Mihir's Important Stuff\\Mihir's Visual Studio\\NHSEE\\Phytoplankton Detection (10)\\2006\\2006\\" + folder
+    direct = str(direct)
+    for (path, dirs, file1) in os.walk(direct):
+        for file in file1:
+                filename = os.path.join(path, file)
+                nlist = []
+                randval = 0
+                image = cv2.imread(filename, 0)
+                try:
+                        analysisframe = cv2.resize(image,(28,28))
+                except:
+                        print(filename)
+                        randval = 1
+
+
+                if randval == 1:
+                        randval = 0
+                        continue
+                else:
+                        for i in range (analysisframe.shape[0]):
+                                for j in range (analysisframe.shape[1]):
+                                        k = analysisframe[i][j]
+                                        k = k/255
+                                        nlist.append(k)
+                        actvalues.append(folder)
+                        pixeldatalist.append(nlist)
+                        nlist = []
+
+
+xtrain = np.array(pixeldatalist)
+ytrain = np.array(actvalues)
+
+print(xtrain)
+print(ytrain)
+
+np.save('rawpixeldata', xtrain)
+np.save('actualvalues', ytrain)
+
+
+# datan.to_csv('bacteria_img_data.csv', index = False)
 # import os
 # import os.path
 # dirs = [d for d in os.listdir("C:\\Users\\milug\\Mihir's Important Stuff\\Mihir's Visual Studio\\NHSEE\\Phytoplankton Detection (10)\\2006\\2006") if os.path.isdir(os.path.join("C:\\Users\\milug\\Mihir's Important Stuff\\Mihir's Visual Studio\\NHSEE\\Phytoplankton Detection (10)\\2006\\2006", d))]
@@ -26,15 +88,3 @@
 
 #     dir_size = 0
 #     numfiles = 0
-
-import cv2
-
-im = cv2.imread("C:\\Users\\milug\\Downloads\\63838cnntoday.png")
-
-gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-gray = cv2.resize(gray,(28,28))
-
-
-for i in range (gray.shape[0]): #traverses through height of the image
-    for j in range (gray.shape[1]): #traverses through width of the image
-        print(gray[i][j])
