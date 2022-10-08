1a57ab017c63a70594ac9eae0ccdbd63c3597407
mg343 <milu.garimella@gmail.com>
2022-10-08 12:02:50 -0400

    shuffled and normalized data for training

--- model.py
+++ model.py
@@ -2,12 +2,24 @@ import numpy as np
 from sklearn import preprocessing
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+import seaborn as sns
+
 
 le = preprocessing.LabelEncoder()
 
 y_train = np.load("actualvalues.npy")
 X_train = np.load("rawpixeldata.npy")
 
+print(len(y_train))
+print(len(X_train))
+
+X_train = X_train[: len(X_train) - 75000]
+y_train = y_train[: len(y_train) - 75000]
+
+X_train, y_train = shuffle(X_train, y_train)
+
+print(y_train)
 # print(np.unique(y_train))
 
 le.fit(y_train)
@@ -17,8 +29,8 @@ y_train = le.transform(y_train)
 df = pd.DataFrame(X_train)
 print(df.head())
 
-img = df.iloc[1000]
-img = img.values
-img = img.reshape((28,28))
-plt.imshow(img,cmap='gray')
-plt.show()
+# Reshape
+# X_train = X_train.values.reshape(-1,28,28,1)
+# test = test.values.reshape(-1,28,28,1)
+# print("x_train shape: ",X_train.shape)
+# print("test shape: ",test.shape)
