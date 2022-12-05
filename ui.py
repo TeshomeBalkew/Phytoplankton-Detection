e8c9beb9198ebb30837f52d217e3e486b70ed251
mg343 <milu.garimella@gmail.com>
2022-12-05 18:14:26 -0500

    integrated input file with model, finished prediction formatting

--- ui.py
+++ ui.py
@@ -3,6 +3,9 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import cv2
 from keras.models import load_model
 import numpy as np
+import pandas as pd
+
+class_names = ['Akashiwo', 'amoeba', 'Amphidinium_sp', 'Asterionellopsis', 'bad', 'bead', 'Cerataulina', 'Cerataulina_flagellate', 'Ceratium', 'Chaetoceros', 'Chaetoceros_didymus', 'Chaetoceros_didymus_flagellate', 'Chaetoceros_flagellate', 'Chaetoceros_other', 'Chaetoceros_pennate', 'Chrysochromulina', 'Ciliate_mix', 'clusterflagellate', 'Cochlodinium', 'Corethron', 'Coscinodiscus', 'Cylindrotheca', 'DactFragCerataul', 'Dactyliosolen', 'Delphineis', 'detritus', 'diatom_flagellate', 'Dictyocha', 'Didinium_sp', 'dino30', 'Dinobryon', 'Dinophysis', 'dino_large1', 'Ditylum', 'Ditylum_parasite', 'Emiliania_huxleyi', 'Ephemera', 'Eucampia', 'Euglena', 'Euplotes_sp', 'flagellate_sp3', 'Guinardia_delicatula', 'Guinardia_flaccida', 'Guinardia_striata', 'Gyrodinium', 'G_delicatula_detritus', 'G_delicatula_external_parasite', 'G_delicatula_parasite', 'Heterocapsa_triquetra', 'Katodinium_or_Torodinium', 'kiteflagellates', 'Laboea_strobila', 'Lauderia', 'Leegaardiella_ovalis', 'Leptocylindrus', 'Leptocylindrus_mediterraneus', 'Licmophora', 'Mesodinium_sp', 'mix_elongated', 'Odontella', 'other_interaction', 'Paralia', 'Parvicorbicula_socialis', 'pennate', 'pennates_on_diatoms', 'pennate_morphotype1', 'Phaeocystis', 'Pleuronema_sp', 'Pleurosigma', 'pollen', 'Prorocentrum', 'Proterythropsis_sp', 'Protoperidinium', 'Pseudochattonella_farcimen', 'Pseudonitzschia', 'Pyramimonas_longicauda', 'Rhizosolenia', 'Skeletonema', 'spore', 'Stephanopyxis', 'Strobilidium_morphotype1', 'Strombidium_capitatum', 'Strombidium_conicum', 'Strombidium_inclinatum', 'Strombidium_morphotype1', 'Strombidium_morphotype2', 'Strombidium_oculatum', 'Thalassionema', 'Thalassiosira', 'Thalassiosira_dirty', 'Tintinnid', 'Tontonia_gracillima', 'zooplankton']
 
 model = load_model('bcnn2014.h5')
 file = 'IFCB1_2006_158_000036_01314 copy.png'
@@ -13,9 +16,19 @@ img = cv2.resize(oimg,(28,28))
 for i in range (img.shape[0]):
         for j in range (img.shape[1]):
                 k = img[i][j]
-                k = k/255
                 file_pixel_data.append(k)
 
-formated_data = file_pixel_data.reshape(-1,28,28,1)
-prediction = model.predict(formated_data)
+formatdata = pd.DataFrame(file_pixel_data).T
+colname = []
+for val in range(784):
+    colname.append(val)
+formatdata.columns = colname
+pixeldata = formatdata.values
+pixeldata = pixeldata / 255
+pixeldata = pixeldata.reshape(-1,28,28,1)
+
+prediction = model.predict(pixeldata)
 predarray = np.array(prediction[0])
+
+classpred = class_names[np.argmax(predarray)]
+print(classpred)
